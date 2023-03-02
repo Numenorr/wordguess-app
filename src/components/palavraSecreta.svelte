f7df901188c4829bd16cfe2a188f5b22a710e42c
Filipe <filipecr2009@gmail.com>
2023-03-02 16:00:22 -0300

    contagem de letras e troca da palavra secreta

---
+++ src/components/palavraSecreta.svelte
@@ -0,0 +1,32 @@
+<script>
+  import Icon from "@iconify/svelte";
+  import { afterUpdate } from "svelte";
+  export let numLetras;
+  export let letras = String;
+  export let acertou = false;
+  let char = [""];
+
+  afterUpdate(() => {
+    char = letras.split("");
+    console.log(char + "  " + acertou);
+  });
+</script>
+
+<div class="grid">
+  {#if acertou === false}
+    {#each Array(numLetras) as _, i}
+      <div><Icon icon="carbon:dot-mark" /></div>
+    {/each}
+  {:else}
+    {#each char as index, i}
+      <div>{index}</div>
+    {/each}
+  {/if}
+</div>
+
+<style>
+  div.grid {
+    display: flex;
+    justify-content: center;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,26 @@
 <script>
   import words from "../words.json";
-  let word = words[1];
+  import Icon from "@iconify/svelte";
+  import PalavraSecreta from "../components/palavraSecreta.svelte";
+
+  let word = words[Math.floor(Math.random() * 21)];
   let guess = "";
   let acertou = false;
   let loading = false;
   let wordDef = null;
+  let chances = 0;
 
   console.log(word);
 
-  async function searchWord() {
+  async function pesquisarPalavra() {
     loading = true;
     wordDef = null;
     acertou = false;
     try {
-      let res = await fetch(
-        "https://api.dictionaryapi.dev/api/v2/entries/en/" + word
-      );
+      let res = await fetch("https://dicio-api-ten.vercel.app/v2/" + word);
       let data = await res.json();
       if (Array.isArray(data)) {
         wordDef = data[0];
-        console.log(wordDef);
       } else {
         wordDef = "No result";
       }
@@ -29,40 +30,90 @@
     }
   }
 
-  function wordTest() {
-    if (guess === word) {
-      acertou = true;
-      console.log("acertou");
-    } else {
-      console.log("tente dnv");
+  async function novaPalavra() {
+    word = words[Math.floor(Math.random() * 28)];
+    chances = 0;
+    acertou = false;
+    try {
+      let res = await fetch("https://dicio-api-ten.vercel.app/v2/" + word);
+      let data = await res.json();
+      if (Array.isArray(data)) {
+        wordDef = data[0];
+      } else {
+        wordDef = "No result";
+      }
+    } catch (err) {
+      loading = false;
     }
+  }
+
+  function adivinhar() {
+    if (chances <= 5)
+      if (guess === word) {
+        acertou = true;
+        console.log("acertou");
+      } else {
+        chances++;
+        console.log("tente dnv" + chances);
+      }
+    else console.log("acabou");
   }
 </script>
 
-<button on:click={searchWord}> New word </button>
-
+<article>
+  <button on:click={novaPalavra}>Nova palavra</button>
+  <PalavraSecreta
+    numLetras={word.length}
+    letras={word}
+    {acertou}
+  />
+</article>
 <form>
   {#if acertou === false}
     <input
       type="text"
       bind:value={guess}
       required
-    />{:else}<input
+    />
+  {:else}
+    <input
       type="text"
       bind:value={guess}
       aria-invalid="false"
       required
-    />{/if}
+    />
+  {/if}
 
   <button
-    on:click={wordTest}
-    type="submit">test</button
+    on:click={adivinhar}
+    type="submit">Adivinhar</button
   >
 </form>
-{#if wordDef !== null}
-  <h1>
-    {wordDef.meanings[0].definitions[Math.floor(Math.random() * 6)].definition}
-  </h1>
-{:else}
-  <h1 aria-busy="true" />
-{/if}
+<Icon icon="basil:check-outline" />
+<Icon icon="basil:cross-outline" />
+<article>
+  <button
+    role="button"
+    class="outline"
+    data-tooltip="Mudar significado da palavra secreta"
+    on:click={pesquisarPalavra}
+  >
+    <Icon
+      icon="mdi:dice-multiple-outline"
+      width="26"
+      height="26"
+    />
+  </button>
+  {#if wordDef !== null}
+    <blockquote>
+      {wordDef.meanings[Math.floor(Math.random() * wordDef.meanings.length)]}
+    </blockquote>
+  {:else}
+    <h1 aria-busy="true" />
+  {/if}
+</article>
+
+<section>
+  Descrições retiradas de
+  <a href="https://www.dicio.com.br/">Dicio</a>.
+</section>
